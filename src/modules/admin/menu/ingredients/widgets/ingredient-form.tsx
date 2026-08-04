4601fa95955578369ed37e953cd3bf58ce145397
Aleksandr Platcev <a.platcev@ya.ru>
2026-08-04 21:35:00 +0300

    fix(admin-ingredients): support empty create form

--- src/modules/admin/menu/ingredients/widgets/ingredient-form.tsx
+++ src/modules/admin/menu/ingredients/widgets/ingredient-form.tsx
@@ -1,18 +1,18 @@
 'use client';
 
 import { JSX } from 'react';
+import { Ingredient } from 'modules/admin/menu/ingredients/entities';
 import { IngredientFormContent } from 'modules/admin/menu/ingredients/features';
 import { FormResetContainer } from 'modules/admin/shared/ui/form-reset-container';
-import { Ingredient } from 'modules/admin/menu/ingredients/entities';
 
 type Props = {
-  ingredient: Ingredient;
+  ingredient?: Ingredient;
 };
 
 export const IngredientForm = ({ ingredient }: Props): JSX.Element => {
   return (
-    <FormResetContainer
-      renderAction={(reset): JSX.Element => <IngredientFormContent ingredient={ingredient} setFormKeyAction={reset} />}
-    />
+    <FormResetContainer>
+      <IngredientFormContent ingredient={ingredient} />
+    </FormResetContainer>
   );
 };
