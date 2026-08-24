311f24058ed8644341096cd9aa1ce03772a400b5
Aleksandr Platcev <a.platcev@ya.ru>
2026-08-24 20:30:56 +0300

    feat(admin-tables): register addon rules entity

--- src/modules/admin/shared/ui/entity-page-template/model/table.config.tsx
+++ src/modules/admin/shared/ui/entity-page-template/model/table.config.tsx
@@ -1,11 +1,13 @@
 import { GridColDef } from '@mui/x-data-grid';
 
 import { Order } from 'modules/admin/orders/entities';
+import { AddonRule } from 'modules/admin/rules/entities';
 import { Product } from 'modules/admin/menu/products/entities';
 import { Category } from 'modules/admin/menu/categories/entities';
 import { Ingredient } from 'modules/admin/menu/ingredients/entities';
 
 import { orderColumns } from 'modules/admin/orders/config';
+import { addonRuleColumns } from 'modules/admin/rules/config';
 import { productColumns } from 'modules/admin/menu/products/config';
 import { categoryColumns } from 'modules/admin/menu/categories/config';
 
@@ -14,6 +16,7 @@ type EntityMap = {
   categories: Category;
   orders: Order;
   products: Product;
+  addonRules: AddonRule;
 };
 
 type TableSlug = keyof EntityMap;
@@ -25,7 +28,7 @@ type TableSchema<T> = {
   };
   searchKey: keyof T;
   href: {
-    create: string;
+    create?: string;
     edit: (id: string | number) => string;
   };
   columns: GridColDef[];
@@ -66,8 +69,7 @@ export const TABLE_CONFIG: { [K in TableSlug]: TableSchema<EntityMap[K]> } = {
     },
     searchKey: 'status',
     href: {
-      create: '/admin/menu/categories/create',
-      edit: (id): string => `/admin/menu/categories/${id}`,
+      edit: (id): string => `/admin/orders/${id}`,
     },
     columns: orderColumns,
   },
@@ -83,62 +85,16 @@ export const TABLE_CONFIG: { [K in TableSlug]: TableSchema<EntityMap[K]> } = {
     },
     columns: productColumns,
   },
+  addonRules: {
+    label: {
+      plural: 'Правила',
+      singular: 'правило',
+    },
+    searchKey: 'title',
+    href: {
+      create: '/admin/rules/create',
+      edit: (id): string => `/admin/rules/${id}`,
+    },
+    columns: addonRuleColumns,
+  },
 };
-
-// export type KeyColumns = keyof typeof TABLE_CONFIG;
-//
-// export const ingredientColumns: GridColDef[] = [
-//   { field: 'id', headerName: 'ID', width: 100 },
-//   { field: 'title', headerName: 'Название', flex: 1, minWidth: 150 },
-// ];
-//
-// export const categoryColumns: GridColDef[] = [
-//   { field: 'id', headerName: 'ID' },
-//   { field: 'image_link', headerName: 'Изображение' },
-//   { field: 'title', headerName: 'Название' },
-//   { field: 'slug', headerName: 'Slug' },
-//   { field: 'is_active', headerName: 'Доступность' },
-//   { field: 'sort_number', headerName: 'Сортировка' },
-// ];
-//
-// export const productColumns: GridColDef[] = [
-//   { field: 'id', headerName: 'ID' },
-//   { field: 'image_link', headerName: 'Изображение' },
-//   { field: 'title', headerName: 'Название' },
-//   { field: 'price', headerName: 'Цена' },
-//   { field: 'weight', headerName: 'Вес' },
-//   { field: 'quantity', headerName: 'Кол-во блюд' },
-//   { field: 'count_portion', headerName: 'Кол-во порций' },
-//   { field: 'slug', headerName: 'Slug' },
-//   { field: 'is_active', headerName: 'Активно' },
-//   { field: 'is_visible', headerName: 'Видимо' },
-//   { field: 'is_set', headerName: 'Сет' },
-// ];
-//
-// export const orderColumns: GridColDef[] = [
-//   { field: 'order_number', headerName: '№ Заказа', width: 100 },
-//   { field: 'formattedDate', headerName: 'Дата', width: 140 },
-//   {
-//     field: 'status',
-//     headerName: 'Статус',
-//     width: 125,
-//     renderCell: (params): JSX.Element => <OrderStatusChip status={params.value} />,
-//   },
-//   {
-//     field: 'orderType',
-//     headerName: 'Тип',
-//     width: 120,
-//   },
-//   {
-//     field: 'total_price',
-//     headerName: 'Сумма',
-//     width: 110,
-//     type: 'number',
-//     align: 'left',
-//     headerAlign: 'left',
-//     valueFormatter: (value): string => `${value} ₽`,
-//   },
-//   { field: 'customer_name', headerName: 'Клиент', width: 150 },
-//   { field: 'customer_phone', headerName: 'Телефон', width: 125 },
-//   { field: 'fullAddress', headerName: 'Адрес доставки', flex: 1 },
-// ];
