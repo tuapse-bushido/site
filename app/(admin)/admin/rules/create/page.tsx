b492695c8ac440eb5c06d554bcef1b0ce6a8a6c9
Aleksandr Platcev <a.platcev@ya.ru>
2026-08-24 20:29:42 +0300

    feat(admin-rules): add create route

---
+++ app/(admin)/admin/rules/create/page.tsx
@@ -0,0 +1,6 @@
+import { JSX } from 'react';
+import { CreateAddonRuleScreen } from 'modules/admin/rules';
+
+export default async function CreateRulePage(): Promise<JSX.Element> {
+  return <CreateAddonRuleScreen />;
+}
